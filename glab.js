794519f8bef98e349b25016e8f40f9dc6fe4db41
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-03-08 17:28:26 -0800

    part four done

--- glab.js
+++ glab.js
@@ -32,17 +32,21 @@ adventurer.roll();
 //  you can add specificity later by extending the classes.
 
 class Character {
-    constructor (name) {
-      this.name = name;
-      this.health = 100;
-      this.inventory = [];
-    }
-    roll(mod = 0) {
-        const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`);
-      }
+  // Add a static MAX_HEALTH property to the Character class, equal to 100.
+  static MAX_HEALTH = 100;
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
   }
-  console.log(Character);
+
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+
+console.log(Character.MAX_HEALTH);
 
 //   Now, we can re-create Robin using the Character class!
 const robin = new Character("Robin");
@@ -59,30 +63,106 @@ console.log(robin.health);
 robin.companion.companion.roll();
 
 // Part 3: Class Features
-// When extending a class, the “child” class inherits
-//  all properties of its parents. This means that we do not need to account for the name, 
-// health, inventory, or roll method of Character children classes.
 
 class Adventurer extends Character {
-    constructor (name, role) {
-      super(name);
-      // Adventurers have specialized roles.
-      this.role = role;
-      // Every adventurer starts with a bed and 50 gold coins.
-      this.inventory.push("bedroll", "50 gold coins");
-    }
-    // Adventurers have the ability to scout ahead of them.
-    scout () {
-      console.log(`${this.name} is scouting ahead...`);
-      super.roll();
-    }
+  // Add a static ROLES array to the Adventurer class, with the values “Fighter,”
+  static ROLES = ["Fighter", "Healer", "Wizard"];
+
+  constructor(name, role, check) {
+    super(name, role);
+    this.role = role;
+    // Add a check to the constructor of the Adventurer class that ensures the given role matches one of these values.
+    this.check = this.check;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+   
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+  restByCampfire() {
+    console.log(`${this.name} Rejuvenates after a long day of adventuring....`);
+    super.roll();
   }
 
-//   Next, create a Companion class with properties and methods specific to the companions.
-// Finally, change the declaration of Robin and the companions to use the new Adventurer and Companion classes.
- class  Companione extends Adventurer(){
-    constructor(name, role) {
-        super()
+  climbMountain() {
+    console.log(`${this.name} Attempts to ascend a mountain....`);
+    super.roll();
+  }
+  addItems(...items) {
+    console.log(this.inventory.push(items));
+    // for (let i=0; i<inventory.length; i++) {
+    //       console.log('this is element i');
+    //       console.log(inventory[i]);
+    // }
+  } 
 
+}
+console.log(Adventurer);
+
+
+console.log(Adventurer.ROLES);
+
+const Halima = new Adventurer("Halima", "Climbing Gear");
+const Halimasy = new Adventurer("Halimasy", "Runner", "I am ok");
+console.log(Halima);
+Halima.climbMountain();
+Halima.restByCampfire();
+console.log(Halimasy);
+Halimasy.climbMountain();
+Halimasy.restByCampfire();
+// Next, create a Companion class with properties and methods specific to the companions.
+class Companion extends Character {
+  constructor(name, type) {
+    super(name);
+    this.name = name;
+    this.type = type;
+  }
+  // Methods  to companion
+  assist() {
+    console.log(`${this.name} is assisting the adventurer.`);
+  }
+  greet() {
+    console.log(`Greetings, fellow adventurer! I am ${this.name}.`);
+  }
+}
+// Finally, change the declaration of Robin and the companions to use
+// the new Adventurer and Companion classes.
+
+let Robin = new Adventurer("Robin" ,"adventurer", "please check on him");
+let Leo = new Companion("Leo", "Cat");
+let Frank = new Companion("Frank", "Flea");
+
+// Create an instance of Companion
+const tinkerbell = new Companion("Tinkerbell", "Fairy");
+tinkerbell.assist();
+tinkerbell.greet();
+
+Robin.addItems("hat");
+console.log(Robin);
+
+// Part 5: Gather your Party
+// Factories are classes that generate objects according to the factory’s instance properties.
+// As an example, let’s look at how we might create many “healer” role adventurers using a factory:
+class AdventurerFactory {  
+    constructor (role) {
+      this.role = role;
+      this.adventurers = [];
     }
- }
+    generate (name) {
+      const newAdventurer = new Adventurer(name, this.role);
+      this.adventurers.push(newAdventurer);
+    }
+    findByIndex (index) {
+      return this.adventurers[index];
+    }
+    findByName (name) {
+      return this.adventurers.find((a) => a.name === name);
+    }
+  }
+  
+  const healers = new AdventurerFactory("Healer");
+  const robin = healers.generate("Robin");
+  
